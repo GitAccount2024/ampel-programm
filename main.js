161e518714e2cf4dc6c3b05b374936ea39359eae
GitAccount2024 <164630306+GitAccount2024@users.noreply.github.com>
2024-03-23 16:31:21 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function main() {
+
+    var input = document.getElementById("inputColor");
+
+    var color = input.value;
+    
+    var lowerColorName = color.toLowerCase();
+
+    if (lowerColorName == "grün") {
+        alert("Du darfst fahren");
+    } else if (lowerColorName == "orange") {
+        alert("Fahren/bremsen");
+    } else if (lowerColorName == "rot") {
+        alert("Du musst halten");
+    } else {
+        alert("Keine gültige Ampel-Farbe eingetragen")
+    }
+}
